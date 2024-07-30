535ca1017a711ca538d0cd7e4158e2991e774800
Saurabh Rai <saurabhrai@Saurabhs-MBP.lan>
2024-07-30 15:48:49 -0700

    Updates

---
+++ precache-manifest.2392cea2ec78a2e3b2da5b6907bb9c1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6224e35834d047778e64d3f846b09ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "88654c0e69954e323a99",
+    "url": "/static/js/main.26948cee.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
