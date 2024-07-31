382bdbef14855fe95c15df80be2575f43d25b0ea
Saurabh Rai <saurabhrai@Saurabhs-MBP.lan>
2024-07-30 17:01:49 -0700

    All component files without css

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const About = () => {
+    return (
+        <section id="about" className="about-me">
+            <h2>About Me</h2>
+            <p>Detailing the professional journey and technical expertise...</p>
+        </section>
+    );
+};
+
+export default About;
